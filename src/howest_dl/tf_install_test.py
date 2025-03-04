84ccc5387091c498ff08cb88506a0f62ef0439e7
samdeleu <sam.deleu.ext@bayer.com>
2025-03-04 10:18:07 +0100

    feat(init): setup base project libs

--- src/howest_dl/tf_install_test.py
+++ src/howest_dl/tf_install_test.py
@@ -1,11 +1,13 @@
-def tf_install_test_():
-    print("Hello from howest-dl!")
-    print("Checking if tensorflow is installed")
+def tf_install_test_2():
+    print("222 Checking if tensorflow is installed")
     import tensorflow as tf
 
-    print("-------------- CPU setup --------------"
-    print(tf.reduce_sum(tf.random.normal([1000, 1000])))"---------------------")
+    print("-------------- CPU setup --------------")
+    print(tf.reduce_sum(tf.random.normal([1000, 1000])))
 
-    print("-------------- GPU setup --------------"
+    print("-------------- GPU setup --------------")
     print(tf.config.list_physical_devices('GPU'))
 
+
+if __name__ == "__main__":
+    tf_install_test_2()
